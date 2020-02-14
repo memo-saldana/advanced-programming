e1684380520875c988c77fe9690d34e403c77f39
José Guillermo Saldaña Cárdenas <memo.sal98@gmail.com>
2020-02-13 20:51:36 -0600

    Fibonacci complete

---
+++ P2/Fibonacci.c
@@ -0,0 +1,49 @@
+//
+// Author: Jose Guillermo Saldana Cardenas
+// ID: A01039888
+// Date: 13/02/2020
+//
+// Show the Fibonacci sequence up to a given amount of numbers, without recursion
+
+#include <stdio.h>
+// #include <iostream.h>
+// using namespace std;
+
+void fibonacci(int iAmount)
+{
+    int iFirst, iSecond, iCount, iCurrentValue;
+
+    if (iAmount < 1)
+    {
+        return;
+    }
+
+    printf("0 ");
+    
+    if (iAmount > 1)
+    {
+        printf(" 1 ");
+    }
+
+    iFirst = 0;
+    iSecond = 1;
+
+    for ( iCount = 3; iCount <= iAmount; iCount++)
+    {
+		iCurrentValue = iFirst + iSecond;
+		printf(" %d ", iCurrentValue);
+		iFirst = iSecond;
+		iSecond = iCurrentValue;
+    }
+}
+
+int main()
+{
+	int iAmount;
+
+    printf( "Input amount of numbers to print.\n" );
+	scanf("%d", &iAmount);
+	fibonacci(iAmount);
+    
+	return 0;
+}
